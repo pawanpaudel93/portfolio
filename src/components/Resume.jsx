a40414d224290fb213e3e2cba309efe6a83ee6c1
Pawan Paudel <pawanpaudel93@gmail.com>
2023-07-10 14:35:42 +0545

    Update resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -14,10 +14,10 @@ export default function Resume() {
               <h4>Pawan Paudel</h4>
               <p>
                 <em>
-                  Passionate, enthusiast, focused and self taught developer with
-                  2+ years of freelance experience working with different web2
-                  technologies and nearly 1 year of experience playing around
-                  with different web3 technologies.
+                  I am a passionate, enthusiast, focused and self taught
+                  developer with over 3 years of freelance experience working
+                  with different web2 technologies. I have also spent over 1
+                  year exploring different web3 technologies.
                 </em>
               </p>
             </div>
@@ -42,35 +42,62 @@ export default function Resume() {
           <div className="col-lg-6">
             <h3 className="resume-title">Working Experience</h3>
             <div className="resume-item">
-              <h4>Freelancer Software Developer</h4>
-              <h5>Apr 2020 - Present</h5>
-              {/* <!-- <p><em>Experion, New York, NY </em></p> --> */}
+              <h4>Part-Time Developer at Longview Labs</h4>
+              <h5>Mar 2023 - Present</h5>
 
               <ul>
                 <li>
-                  Worked on different Python Automation projects using Selenium
-                  & created dashboard with Django for all those projects.
+                  Developed a web app called{" "}
+                  <a href="https://ella.social">ella.social</a> that permanently
+                  backs up Instagram photos to{" "}
+                  <a href="https://arweave.org/">Arweave</a>.
                 </li>
                 <li>
-                  Created a web app to control the different servers and the
-                  automation with Flask APIs and VueJS.
+                  Created a web app called{" "}
+                  <a href="https://dragondeploy.xyz/">dragondeploy</a> with{" "}
+                  <a href="https://ar.io/arns/">ArNS</a> support, allowing users
+                  to deploy web apps to{" "}
+                  <a href="https://arweave.org/">Arweave</a>. Also wrote Python
+                  code to display deployment statistics using GitHub Actions.
                 </li>
                 <li>
-                  Worked on a project called HeliumControl created using Django
-                  Rest Framework (DRF) and VueJS to manage and visualize the
-                  stats of different helium hotspots.
+                  Developed a web app called{" "}
+                  <a href="https://mediamoat.vercel.app/">Media Moat</a> that
+                  uploads photos permanently to{" "}
+                  <a href="https://arweave.org/">Arweave</a> using{" "}
+                  <a href="https://othent.io/">Othent</a>.
+                </li>
+              </ul>
+            </div>
+            <div className="resume-item">
+              <h4>Freelancer Software Developer</h4>
+              <h5>Apr 2020 - Mar 2023</h5>
+
+              <ul>
+                <li>
+                  Worked on various Python Automation projects using Selenium
+                  and created a dashboard with Django to display statistics for
+                  all those projects.
+                </li>
+                <li>
+                  Developed a web app using Flask APIs and VueJS to control
+                  different servers running Selenium automation code.
+                </li>
+                <li>
+                  Created <a href="https://heliumcontrol.com/">HeliumControl</a>
+                  , a project using Django Rest Framework (DRF) and VueJS, to
+                  manage and visualize statistics of different Helium hotspots.
                 </li>
               </ul>
             </div>
             <div className="resume-item">
               <h4>Freelance at Freelancer.com</h4>
               <h5>Jan 2020 - Apr 2020</h5>
-              {/* <!-- <p><em>Stepping Stone Advertising, New York, NY</em></p> --> */}
 
               <ul>
                 <li>
-                  Worked on different projects related to Python, Django,
-                  JavaScript, Machine Learning etc
+                  Worked on various projects related to Python, Django,
+                  JavaScript, Machine Learning, and more.
                 </li>
               </ul>
             </div>

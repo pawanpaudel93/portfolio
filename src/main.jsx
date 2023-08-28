987bf40c50626303dddd5e375ebde307ea342f8c
Pawan Paudel <pawanpaudel93@gmail.com>
2023-08-28 17:06:42 +0545

    chore: Update packages

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./css/styles.scss";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "remixicon/fonts/remixicon.css";
 import "swiper/css";
 import "./css/style.css";
 import * as bootstrap from "bootstrap";
